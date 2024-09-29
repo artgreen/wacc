d177bb18d66c2d350664c6d30a51087fdc8e998b
green <25813+artgreen@users.noreply.github.com>
2024-09-29 13:30:10 -0500

    cleaned up hexdump

--- debug.asm
+++ debug.asm
@@ -7,14 +7,9 @@
 
 hexdump     start
             sub     (4:address,2:count),0
-            lda     count
-            lda     address
-            lda     address+2
-            pha
-            lda     address
-            pha
-            lda     count
-            pha
+            pei     address+2
+            pei     address
+            pei     count
             ldx     #$0fff
             jsl     $e10000
             ret
