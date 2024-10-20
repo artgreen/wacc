1bab39bb9d3e7c9ec83052388aa8bc8f43b13659
green <25813+artgreen@users.noreply.github.com>
2024-10-20 16:19:29 -0500

    quiet down debug.asm

--- debug.asm
+++ debug.asm
@@ -1,9 +1,11 @@
 
-            list    on
+            list    off
             gen     on
             65816   on
             mcopy   2:ORCAInclude:m16.ORCA
             mcopy   m16.utils.asm
+
+            trace off
 
 hexdump     start
             sub     (4:address,2:count),0
